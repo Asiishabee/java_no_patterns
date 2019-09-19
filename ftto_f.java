e33ee7050fa48f6770a22ea607674e9293daaede
Asiishabee <42670433+Asiishabee@users.noreply.github.com>
2019-09-19 16:20:54 +0530

    Create ftto_f.java

---
+++ ftto_f.java
@@ -0,0 +1,16 @@
+public class ftto_f
+{
+	public static void main(String[] args)
+	{
+	    for(int i=1;i<=4;i++)
+	    {
+    	    for(int j=4;j>=i;j--)
+    	    {
+        	    
+        		System.out.print(j);
+        		
+    	    }
+        	    System.out.println("");
+	    }
+	}
+}
